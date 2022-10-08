c19407c0f88836583d0286c8038522c581a283ae
Poco <102257120+pococdn@users.noreply.github.com>
2022-10-08 16:06:49 -0500

    Update anime1.js

--- anime1.js
+++ anime1.js
@@ -32,12 +32,12 @@ document.getElementById('anime--import').innerHTML = `
     <div class="cont_anime-container">
         <span class="anime-title">`+titulo+`</span>
         <strong class="anime-sinopsis" style="text-align: justify;">`+sinopsis+`</strong>
-        <div class="anime-tags">`+genero[0]+genero[1]+genero[2]+genero[3]+genero[4]+genero[5]+`</div>
+        <div class="anime-tags">`+genero.toString().replace(new RegExp(",","gi"),"")+`</div>
     </div>
 </div>
 <div class="status--anime `+estado+`"></div>
 <div class="episode-text">Lista de episodios</div>
 <div class="episode-container">
-    <ul class="list-episodes">`+episodio[1]+episodio[2]+episodio[3]+episodio[4]+episodio[5]+episodio[6]+episodio[7]+episodio[8]+episodio[9]+episodio[10]+episodio[11]+episodio[12]+episodio[13]+episodio[14]+episodio[15]+episodio[16]+episodio[17]+episodio[18]+episodio[19]+episodio[20]+episodio[21]+episodio[22]+episodio[23]+episodio[24]+episodio[25]+episodio[26]+episodio[27]+episodio[28]+episodio[29]+episodio[30]+episodio[31]+episodio[32]+episodio[33]+episodio[34]+episodio[35]+episodio[36]+episodio[37]+episodio[38]+episodio[39]+episodio[40]+episodio[41]+episodio[42]+episodio[43]+episodio[44]+episodio[45]+episodio[46]+episodio[47]+episodio[48]+episodio[49]+episodio[50]+`</ul>
+    <ul class="list-episodes">`+episodio.toString().replace(new RegExp(",","gi"),"")+`</ul>
 </div>
 `;

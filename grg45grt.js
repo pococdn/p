8190e253444aab770a8cb2996ff060a0d612efc8
Poco <102257120+pococdn@users.noreply.github.com>
2022-10-08 09:16:54 -0500

    Update grg45grt.js

--- grg45grt.js
+++ grg45grt.js
@@ -3,7 +3,7 @@ for(x=1;x < url.length;x++){
     <a href="`+url[x]+`"><li class="emision-content">`+anime_emision[x]+`<span>ANIME</span></li></a>
     `;
 }
-var emision_list__get = emision_list.toString().replace(',','');
+var emision_list__get = emision_list.toString().replace(new RegExp(",","gi"),"");
 document.getElementById('import--emision').innerHTML = `
     <ul class="emision-target">
         `+emision_list__get+`

49f1f76d8408b6d69ae027f4df42ed7fcd251958
Poco <102257120+pococdn@users.noreply.github.com>
2022-10-08 16:37:48 -0500

    Update anime1.js

--- anime1.js
+++ anime1.js
@@ -20,7 +20,7 @@ for(e = 1;e < titulo_episodio.length; e++){
 };
 if(titulo_episodio[0] != undefined){
     if(episodio[1] = undefined){
-    episodio[episodio.length] = `
+    episodio[titulo_episodio.length] = `
 <a href="#"><li class="link-episode border">
                     <div class="episode-img">
                         <div class="banner-img"><img src="https://i.imgur.com/nzVuY3z.jpeg"></div>

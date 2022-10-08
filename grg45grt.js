16a6e224b03afcde1969ff0f1de838b24817dcf4
Poco <102257120+pococdn@users.noreply.github.com>
2022-10-07 23:54:16 -0500

    Update grg45grt.js

--- grg45grt.js
+++ grg45grt.js
@@ -1,6 +1,6 @@
 for(x=1;x < url.length;x++){
     emision_list[x] = `
-    <a href="`+url[x]+`"><li class="emision-content">`+anime_emision[x]+`<span>`+fecha_emision[x]+`</span></li></a>
+    <a href="`+url[x]+`"><li class="emision-content">`+anime_emision[x]+`<span>ANIME</span></li></a>
     `;
 }
 var emision_list__get = emision_list.toString().replace(',','');
